647f0038c9519a3cce1666dee0db3c44716eb9c6
PranavBP <bellam.pranav@gmail.com>
2024-01-11 23:18:39 -0500

    Initial Commit

---
+++ Fetch-Recipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch-Recipe
+//
+//  Created by Pranav Bellam Prasad on 1/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch-Recipe/Fetch_RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_RecipeApp.swift
+//  Fetch-Recipe
+//
+//  Created by Pranav Bellam Prasad on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
